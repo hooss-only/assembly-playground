e21da675e6d2670b53042ae18b2ca46dc5cd7e1d
Changmin Jung <hoosshusu@gmail.com>
2026-07-19 21:50:38 +0900

    Add print_str

---
+++ print_str/print_str.s
@@ -0,0 +1,31 @@
+.global _print_str
+
+// print_str(char* str)
+_print_str:
+  stp x29, x30, [sp, #-16]!
+  mov x29, sp
+
+  mov x9, x0 // x9: char* str
+  mov x11, x9 // x11: char* tmp = str
+
+.Lget_str_len:
+  mov w10, #0 // x10: int length = 0
+
+.Lget_len_loop:
+  ldrb w12, [x11] // x12: char c = *str
+  cbz x12, .Lwrite
+  add w10, w10, #1
+  add x11, x11, #1
+  b .Lget_len_loop
+
+.Lwrite:
+  mov x0, #0
+  mov x1, x9
+  mov x2, x10
+
+  mov x16, #4
+  svc #0
+
+.Lreturn:
+  ldp x29, x30, [sp], #16
+  ret
---
+++ print_str/test.c
@@ -0,0 +1,7 @@
+extern void print_str(char* str);
+
+int main(void) {
+        char test[] = "Hello, World!\n";
+        print_str(test);
+        return 0;
+}
